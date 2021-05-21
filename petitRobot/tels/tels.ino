e5b367ab5f94c437062e2682c0643a0869bc0110
DamienDerr <damienderr84@gmail.com>
2021-05-21 15:05:24 +0200

    maj tels

--- petitRobot/tels/tels.ino
+++ petitRobot/tels/tels.ino
@@ -1,15 +1,28 @@
 
-int valeur; // 0 quand leds allumées
+int ArG; // 0 quand leds allumées == obstacle détecté
+int ArD;
+int pin_ArG = 4;
+int pin_ArD = 5;
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(3, INPUT);
   Serial.begin(9600);
+  
+  pinMode(pin_ArG, INPUT);
+  pinMode(pin_ArD, INPUT);
+  
 }
 
 void loop() {
-  valeur = digitalRead(3);
-  Serial.print("valeur : ");
-  Serial.println(valeur);
-  delay(100);
+  ArG = digitalRead(pin_ArG);
+  ArD = digitalRead(pin_ArD);
+  
+  Serial.print("Gauche : ");
+  Serial.println(ArG);
+  
+  Serial.println("");
+  
+  Serial.print("Droite : ");
+  Serial.println(ArD);
+  
+  delay(500);
 }
